6ef044ef03eaaa275d5a4a687b9f287c24077134
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-17 15:19:50 +0200

    Boolflix : Starting

--- src/store.js
+++ src/store.js
@@ -1,1 +1,14 @@
-// Qui vanno i dati
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint : {
+        movie: 'search/movie',
+        tv: 'search/tv'
+    },
+    options: {
+        api_key: '8c01fefc2763c1d51cbd748c17c53732',
+        query: 'a'
+    },
+    imageUrl: 'http://image.tmdb.org/t/p/original'
+})
